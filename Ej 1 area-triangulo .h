7a326631a7e80d132a04a3d39ccf26f150a019f6
camiladiazjus <99283217+camiladiazjus@users.noreply.github.com>
2022-02-22 21:09:54 -0400

    Create Ej 1 area-triangulo .h

---
+++ Ej 1 area-triangulo .h
@@ -0,0 +1,14 @@
+Class triangulo
+	{
+	
+	private:
+		float base;
+		float altura;
+		float area;
+		//Metodos - Funciones
+	public:
+		triangulo(void); 
+		~triangulo(void); 
+		//Prototipos
+		float calculararea(float b, float h);
+	};
